4000f6adf722c786c16cbc9aedbd2cda4c57b0ee
John H. Watson <klymov@email.cz>
2024-10-04 01:05:44 +0200

    NEW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,112 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+class Zbozi {
+    private int id;
+    private String nazev;
+    private double cena;
+    private int pocetKs;
+
+    public Zbozi(int id, String nazev, double cena, int pocetKs) {
+        this.id = id;
+        this.nazev = nazev;
+        this.cena = cena;
+        this.pocetKs = pocetKs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public int getPocetKs() {
+        return pocetKs;
+    }
+
+    public void setPocetKs(int pocetKs) {
+        this.pocetKs = pocetKs;
+    }
+
+    public double getCelkovaCena() {
+        return cena * pocetKs;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-10d %-20s %-10.2f %-10d", id, nazev, cena, pocetKs);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Sklad sklad = new Sklad();
+        Scanner scanner = new Scanner(System.in);
+        int volba;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("\n1. Přidat nebo aktualizovat zboží");
+            System.out.println("2. Aktualizovat počet kusů zboží podle ID");
+            System.out.println("3. Vypsat zboží podle ID");
+            System.out.println("4. Zrušit zboží podle ID");
+            System.out.println("5. Vypsat všechna zboží seřazená podle ID");
+            System.out.println("6. Spočítat celkovou cenu zboží");
+            System.out.println("7. Konec");
+            System.out.print("Vyberte operaci: ");
+            volba = scanner.nextInt();
+
+            switch (volba) {
+                case 1:
+                    System.out.print("Zadejte ID zboží: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();  // Vyčistit buffer
+                    System.out.print("Zadejte název zboží: ");
+                    String nazev = scanner.nextLine();
+                    System.out.print("Zadejte cenu zboží: ");
+                    double cena = scanner.nextDouble();
+                    System.out.print("Zadejte počet kusů: ");
+                    int pocetKs = scanner.nextInt();
+                    sklad.pridatZbozi(id, nazev, cena, pocetKs);
+                    break;
+                case 2:
+                    System.out.print("Zadejte ID zboží: ");
+                    int idAkt = scanner.nextInt();
+                    System.out.print("Zadejte nový počet kusů: ");
+                    int novyPocetKs = scanner.nextInt();
+                    sklad.aktualizovatPocetZbozi(idAkt, novyPocetKs);
+                    break;
+                case 3:
+                    System.out.print("Zadejte ID zboží: ");
+                    int idVypis = scanner.nextInt();
+                    sklad.vypisZbozi(idVypis);
+                    break;
+                case 4:
+                    System.out.print("Zadejte ID zboží: ");
+                    int idZrus = scanner.nextInt();
+                    sklad.zrusitZbozi(idZrus);
+                    break;
+                case 5:
+                    sklad.vypisVsechny();
+                    break;
+                case 6:
+                    System.out.println("Celková cena všech zboží ve skladu: " + sklad.vypocetCelkoveCeny());
+                    break;
+                case 7:
+                    System.out.println("Ukončuji program.");
+                    break;
+                default:
+                    System.out.println("Neplatná volba, zkuste to znovu.");
+            }
+        } while (volba != 7);
+
+        scanner.close();
     }
 }
--- src/Sklad.java
+++ src/Sklad.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class Sklad {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Sklad {
+    private Map<Integer, Zbozi> sklad;
+
+    public Sklad() {
+        sklad = new HashMap<>();
+    }
+
+    public void pridatZbozi(int id, String nazev, double cena, int pocetKs) {
+        Zbozi zbozi = new Zbozi(id, nazev, cena, pocetKs);
+        sklad.put(id, zbozi);
+        System.out.println("Zboží přidáno/aktualizováno.");
+    }
+
+    public void aktualizovatPocetZbozi(int id, int pocetKs) {
+        Zbozi zbozi = sklad.get(id);
+        if (zbozi != null) {
+            zbozi.setPocetKs(pocetKs);
+            if (pocetKs <= 0) {
+                sklad.remove(id);
+                System.out.println("Zboží s ID " + id + " bylo odstraněno ze skladu (počet kusů = 0).");
+            } else {
+                System.out.println("Počet kusů zboží s ID " + id + " byl aktualizován.");
+            }
+        } else {
+            System.out.println("Zboží s ID " + id + " nebylo nalezeno.");
+        }
+    }
+
+    public void vypisZbozi(int id) {
+        Zbozi zbozi = sklad.get(id);
+        if (zbozi != null) {
+            System.out.println(zbozi);
+        } else {
+            System.out.println("Zboží s ID " + id + " nebylo nalezeno.");
+        }
+    }
+
+    public void zrusitZbozi(int id) {
+        if (sklad.remove(id) != null) {
+            System.out.println("Zboží s ID " + id + " bylo zrušeno.");
+        } else {
+            System.out.println("Zboží s ID " + id + " nebylo nalezeno.");
+        }
+    }
+
+    public void vypisVsechny() {
+        TreeMap<Integer, Zbozi> serazenySklad = new TreeMap<>(sklad);
+
+        System.out.println(String.format("%-10s %-20s %-10s %-10s", "ID", "Název", "Cena", "Počet kusů"));
+        System.out.println("--------------------------------------------------------------");
+        for (Zbozi zbozi : serazenySklad.values()) {
+            System.out.println(zbozi);
+        }
+    }
+
+    public double vypocetCelkoveCeny() {
+        double celkovaCena = 0;
+        for (Zbozi zbozi : sklad.values()) {
+            celkovaCena += zbozi.getCelkovaCena();
+        }
+        return celkovaCena;
+    }
 }
